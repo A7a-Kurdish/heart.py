5c25b895b17c993244e516f40b748fc5b3cf33c3
A7a_Kurdish <69413932+A7a-Kurdish@users.noreply.github.com>
2022-09-19 17:13:04 +0300

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,34 @@
+from tkinter import font
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.bgcolor('black')
+t.hideturtle()
+t.goto (0,-10)
+
+t.pensize(3)
+t.color('red')
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle (-90,200)
+t.setheading (60)
+t.circle (-90,200)
+t.forward (178)
+t.end_fill ()
+
+t.penup()
+t.goto(-85,130)
+t.pendown()
+t.color('white')
+t.write("I Love You",font=("Verdana",25,"bold"))
+
+t.penup()
+t.goto(-220,-180)
+t.pendown()
+t.color('red')
+t.write("   A7a Kurdish",font=("Verdana",30,"bold"))
+
+turtle.done()
